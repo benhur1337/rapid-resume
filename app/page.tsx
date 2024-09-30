548a88881d0cf9a939af05c3c8aa3a77e7b0bca8
benhur1337 <papabenny420@gmail.com>
2024-09-30 12:09:09 +0700

    Added new changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 export default function Home(){
   return(
-    <div></div>
+    <div>
+      <section className="flex flex-col gap-4 justify-center items-start md:items-center h-[500px] w-screen p-6 duration-500">
+        <strong className="text-5xl tracking-tighter">Rapidly Build Your Resume.</strong>
+        <h4>The free, no bs, resume builder.</h4>
+        <div>
+          <button className="border bg-white text-black rounded p-2">Get Started</button>
+        </div>
+      </section>
+    </div>
   )
 }
